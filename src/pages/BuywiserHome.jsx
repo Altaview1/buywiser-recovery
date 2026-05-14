075a48f268f94c5e6e2c5ce2933081b839605159
bennett <bennett@buywiser.com>
2026-05-14 01:09:02 +0000

    File changes

--- src/pages/BuywiserHome.jsx
+++ src/pages/BuywiserHome.jsx
@@ -664,7 +664,6 @@ function LandingView() {
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
               style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
             />
           </div>
