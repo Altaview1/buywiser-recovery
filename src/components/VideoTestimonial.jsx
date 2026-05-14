27625b2fc328069f47f7e074ad47129b67aca3a6
bennett <bennett@buywiser.com>
2026-05-14 08:13:24 +0000

    File changes

--- src/components/VideoTestimonial.jsx
+++ src/components/VideoTestimonial.jsx
@@ -1,16 +1,14 @@
 export default function VideoTestimonial() {
   return (
-    <div style={{ width: "100%", borderRadius: "16px", overflow: "hidden", background: "#000" }}>
+    <div className="relative w-full pb-[56.25%] rounded-xl overflow-hidden bg-black">
       <iframe
-        width="100%"
-        height="315"
-        src="https://www.youtube.com/embed/ZKUbVLVpzMw?si=F_zqvWOQmJ0NKifh"
+        className="absolute inset-0 w-full h-full"
+        src="https://www.youtube.com/embed/ZKUbVLVpzMw?si=F_zqvWOQmJ0NKifh&enablejsapi=1"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
-        style={{ display: "block" }}
       />
     </div>
   );
